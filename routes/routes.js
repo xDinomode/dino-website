aebc43c72f2e2c4fbb447690880c8978ccc924ce
xDinomode <xDinomode@users.noreply.github.com>
2016-01-12 11:37:36 -0600

    added a new lesson /tutorials/node/1

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,17 @@ var router = express.Router();
 router.get("/", function(req, resp){
   resp.render("homepage");
 });
-router.get("/tutorials/node/1", function(req, resp){
-  resp.render("node");
+router.get("/tutorials/node/:number", function(req, resp){
+  switch(req.params.number){
+    case("1"):
+    resp.render("./node/node1");
+    break;
+    case("2"):
+    resp.render("./node/node2");
+    break;
+    default:
+    resp.redirect("/");
+  }
 });
 router.get("/contact", function(req, resp){
   resp.render("contact");
