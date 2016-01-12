47cebf26872b53d48ed5d2a42b0abec75edc5104
xDinomode <xDinomode@users.noreply.github.com>
2016-01-11 20:21:08 -0600

    added a home page and icons

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, resp){
-  resp.render("index");
+  resp.render("homepage");
 });
-
+router.get("/tutorials/node/1", function(req, resp){
+  resp.render("node");
+});
+router.get("/contact", function(req, resp){
+  resp.render("contact");
+});
 module.exports = router;
